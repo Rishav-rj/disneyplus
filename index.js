f78bcd9732f89940f87aa0c792a0209520a1528f
Rishav Jaiswal <95870745+Rishav-rj@users.noreply.github.com>
2022-12-09 00:06:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
-const Cors = require("Cors");
+// const Cors = require("Cors");
 const path = require('path');
 require('dotenv').config();
 
 app.use(express.json());
-app.use(Cors());
+// app.use(Cors());
 
 mongoose.connect(process.env.MONGODB,
 {
